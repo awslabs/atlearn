0bc7584ddd54f73d48c1c356b36f4b5200aadaeb
KexinFeng <fenkexin@amazon.com>
2023-01-27 18:45:53 -0800

    comments on yolov5

--- ATLearn/utils/yolo_data_loader.py
+++ ATLearn/utils/yolo_data_loader.py
@@ -157,6 +157,7 @@ def verify_image_label(args):
         else:
             nm = 1  # label missing
             lb = np.zeros((0, 5), dtype=np.float32)
+            os.remove(im_file)
         return im_file, lb, shape, segments, nm, nf, ne, nc, msg
     except Exception as e:
         nc = 1
@@ -270,23 +271,41 @@ class LoadImagesAndLabels(Dataset):
         x = {}  # dict
         nm, nf, ne, nc, msgs = 0, 0, 0, 0, []  # number missing, found, empty, corrupt, messages
         desc = f"{prefix}Scanning '{path.parent / path.stem}' images and labels..."
-        with Pool(NUM_THREADS) as pool:
-            pbar = tqdm(pool.imap(verify_image_label, zip(self.im_files, self.label_files, repeat(prefix))),
-                        desc=desc,
-                        total=len(self.im_files),
-                        bar_format=BAR_FORMAT)
-            for im_file, lb, shape, segments, nm_f, nf_f, ne_f, nc_f, msg in pbar:
-                nm += nm_f
-                nf += nf_f
-                ne += ne_f
-                nc += nc_f
-                if im_file:
-                    x[im_file] = [lb, shape, segments]
-                if msg:
-                    msgs.append(msg)
-                pbar.desc = f"{desc}{nf} found, {nm} missing, {ne} empty, {nc} corrupt"
 
-        pbar.close()
+        # with Pool(NUM_THREADS) as pool:
+        #     pbar = tqdm(pool.imap(verify_image_label, zip(self.im_files, self.label_files, repeat(prefix))),
+        #                 desc=desc,
+        #                 total=len(self.im_files),
+        #                 bar_format=BAR_FORMAT)
+        #     for im_file, lb, shape, segments, nm_f, nf_f, ne_f, nc_f, msg in pbar:
+        #         nm += nm_f
+        #         nf += nf_f
+        #         ne += ne_f
+        #         nc += nc_f
+        #         if im_file:
+        #             x[im_file] = [lb, shape, segments]
+        #         if msg:
+        #             msgs.append(msg)
+        #         pbar.desc = f"{desc}{nf} found, {nm} missing, {ne} empty, {nc} corrupt"
+        # pbar.close()
+
+        it = (verify_image_label(item) for item in zip(self.im_files, self.label_files, repeat(prefix)))
+        pbar = tqdm(it,
+                    desc=desc,
+                    total=len(self.im_files),
+                    bar_format=BAR_FORMAT)
+
+        for im_file, lb, shape, segments, nm_f, nf_f, ne_f, nc_f, msg in pbar:
+            nm += nm_f
+            nf += nf_f
+            ne += ne_f
+            nc += nc_f
+            if im_file:
+                x[im_file] = [lb, shape, segments]
+            if msg:
+                msgs.append(msg)
+            pbar.desc = f"{desc}{nf} found, {nm} missing, {ne} empty, {nc} corrupt"
+
         x['hash'] = get_hash(self.label_files + self.im_files)
         x['results'] = nf, nm, ne, nc, len(self.im_files)
         x['msgs'] = msgs  # warnings
